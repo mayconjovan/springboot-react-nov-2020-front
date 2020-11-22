a1b9db3807214b37c9b919340fa39410c3a18a74
Maycon J Pereira <mayconjovan@gmail.com>
2020-11-22 12:59:11 -0300

    Validations singup user

--- src/views/cadastroUsuario.js
+++ src/views/cadastroUsuario.js
@@ -2,6 +2,8 @@ import React from 'react'
 import Card from '../components/card'
 import FormGroup from '../components/form-group'
 import {withRouter} from 'react-router-dom'
+import UsuarioService from '../app/service/usuarioService'
+import {mensagemErro, mensagemSucesso} from '../components/toastr'
 
 class CadastroUsuario extends React.Component {
     state = {
@@ -11,8 +13,55 @@ class CadastroUsuario extends React.Component {
         senhaRepeticao: ''
     }
 
+    constructor(){
+        super();
+        this.service = new UsuarioService();
+    }
+
+    validar(){
+        const msgs = []
+        if(!this.state.nome) {
+            msgs.push('O campo nome é obrigatório.')
+        }
+        
+        if(!this.state.email) {
+            msgs.push('O campo email é obrigatório.')
+        }else if(!this.state.email.match(/^[a-z0-9.]+@[a-z0-9]+\.[a-z]/)){
+            msgs.push('Informe um email válido.')
+        }
+
+        if(!this.state.senha || !this.state.senhaRepeticao) {
+            msgs.push('Digite a senha duas vezes.')
+
+        }else if(this.state.senha !== this.state.senhaRepeticao) {
+            msgs.push('As senhas devem ser iguais.')
+        }
+
+        return msgs;
+    }
+
     cadastrar = () => {
-        console.log(this.state)
+        const msgs = this.validar();
+
+        if(msgs && msgs.length > 0) {
+            msgs.forEach( (msg, index) => {
+                mensagemErro(msg);
+            });
+            return false;
+        }
+
+        const usuario = {
+            nome: this.state.nome,
+            email: this.state.email,
+            senha: this.state.senha
+        }
+        this.service.salvar(usuario)
+            .then(response => {
+                mensagemSucesso('Cadastrado efetuado com sucesso!')                
+                this.props.history.push('/login')
+            }).catch(error => {
+                mensagemErro(error.response.data)
+            })
     }
 
     cancelar = () => {
