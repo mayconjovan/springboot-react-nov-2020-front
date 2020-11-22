45ac0c97829e821cfd2619c6ae8053eedbd96b2a
Maycon J Pereira <mayconjovan@gmail.com>
2020-11-22 12:10:40 -0300

    Add toastr messages

--- src/views/Login.js
+++ src/views/Login.js
@@ -6,13 +6,15 @@ import { withRouter } from 'react-router-dom'
 import UsuarioService from '../app/service/usuarioService'
 import LocalStorageService from '../app/service/localStorageService'
 
+import {mensagemErro} from '../components/toastr'
+
 
 class Login extends React.Component {
 
     state = {
         email: '',
         senha: '',
-        mensagemErro: null
+      
     }
 
     constructor() {
@@ -28,7 +30,7 @@ class Login extends React.Component {
                 LocalStorageService.addItem('_usuario_logado', response.data)             
                 this.props.history.push('/home')                
             }).catch(erro => {
-                this.setState({ mensagemErro: erro.response.data })
+                mensagemErro(erro.response.data)
             })
     }
 
@@ -40,10 +42,7 @@ class Login extends React.Component {
             <div className="row">
                 <div className="col-md-6" style={{ position: 'relative', left: '300px' }}>
                     <div className="bs-docs-section">
-                        <Card title="Login">
-                            <div className="row">
-                                <span>{this.state.mensagemErro}</span>
-                            </div>
+                        <Card title="Login">                            
                             <div className="row">
                                 <div className="col-lg-12">
                                     <div className="bs-component">
